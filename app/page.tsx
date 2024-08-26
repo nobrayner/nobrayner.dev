7fde4495aa5489bdd2b9d9ba278a6e783fa66759
Braydon Hall <40751395+nobrayner@users.noreply.github.com>
2024-08-26 14:39:02 +0200

    feat: basic personal site

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,160 @@
-import Image from "next/image";
+export const dynamic = "force-dynamic";
 
 export default function Home() {
+  const now_string = new Date().toLocaleDateString(undefined, {
+    timeZone: "UTC",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+
+  const ttys_num = Math.floor(Math.random() * 1000)
+    .toString()
+    .padStart(3, "0");
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="flex justify-center min-h-screen p-4 md:p-32">
+      <div className="max-w-fit rounded-lg h-fit overflow-none">
+        <div className="flex bg-white/20 gap-1 p-2 rounded-t-lg items-center">
+          <div className="w-3 h-3 bg-red-400 rounded-full" />
+          <div className="w-3 h-3 bg-yellow-400 rounded-full" />
+          <div className="w-3 h-3 bg-green-400 rounded-full" />
+          <h1 className="text-xs text-white/70 flex-grow text-center">
+            nobrayner.dev
+          </h1>
+          <div className="w-3 h-3" />
+          <div className="w-3 h-3" />
+          <div className="w-3 h-3" />
         </div>
+        <main className="p-4 bg-black rounded-b-lg border border-white/40 border-t-0">
+          <p className="text-white/60">
+            Last login: {now_string} on ttys{ttys_num}
+          </p>
+          <br />
+          <Prompt command="whoami" />
+          <WhoamiCommand />
+          <br />
+          <Prompt command="ls -l" />
+          <ListCommand />
+        </main>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
+
+function Prompt({ command }: { command: string }) {
+  return (
+    <div className="flex">
+      <p className="text-white/70">
+        <span className="text-yellow-300/70">$</span> guest@nobrayner.dev:
+      </p>
+      <p>&nbsp;{command}</p>
+    </div>
+  );
+}
+
+function WhoamiCommand() {
+  return (
+    <>
+      <p>Braydon Hall, Software Engineer based in Australia</p>
+    </>
+  );
+}
+
+function ListCommand() {
+  return ITEMS.map((item) => <ListItem {...item} />);
+}
+
+const ITEMS: Item[] = [
+  {
+    type: "link",
+    name: "LinkedIn",
+    count: 1,
+    style: "text-blue-400",
+    date: "2024-08-26T13:29:00.000Z",
+    size: 51,
+    url: "https://www.linkedin.com/in/braydon-hall-6aba72200/",
+  },
+  {
+    type: "link",
+    name: "GitHub",
+    count: 7,
+    style: "text-gray-300",
+    date: "2018-07-02T00:00:00.000Z",
+    size: 28,
+    url: "https://github.com/nobrayner",
+  },
+  {
+    type: "file",
+    name: "resume.pdf",
+    count: 5,
+    style: "text-gray-400",
+    date: "2024-06-10T12:43:00.000Z",
+    size: 278094,
+    url: "/Braydon Hall - Software Engineer.pdf",
+  },
+  {
+    type: "link",
+    name: "Contact me",
+    count: 364,
+    style: "text-green-400",
+    date: "2024-01-01T00:00:00.000Z",
+    size: 0,
+    url: "mailto:braydon@nobrayner.dev",
+  },
+];
+
+const LONGEST_ITEM_CHARS = Math.max(
+  ...ITEMS.map((i) => i.size.toString().length),
+);
+const LONGEST_ITEM_COUNT = Math.max(
+  ...ITEMS.map((i) => i.count.toString().length),
+);
+
+function ListItem({
+  type,
+  name,
+  count,
+  style,
+  size,
+  date: date_string,
+  url,
+}: Item) {
+  const date = new Date(date_string);
+  const six_months_ago = new Date();
+  six_months_ago.setMonth(six_months_ago.getMonth() - 6);
+
+  return (
+    <div>
+      <a href={url}>
+        <pre className="inline whitespace-break-spaces">
+          {TYPE_MAP[type]} {count.toString().padStart(LONGEST_ITEM_COUNT, " ")}{" "}
+          nobrayner visitors {size.toString().padStart(LONGEST_ITEM_CHARS, " ")}{" "}
+          {date.getDate().toString().padStart(2, " ")}{" "}
+          {date.toLocaleString(undefined, {
+            month: "short",
+          })}{" "}
+          {date.getTime() > six_months_ago.getTime()
+            ? `${date.getHours().toString().padStart(2, "0")}:${date.getMinutes().toString().padStart(2, "0")}`
+            : date.getFullYear().toString().padStart(5, " ")}{" "}
+          <span className={style}>{name}</span>
+        </pre>
+      </a>
+    </div>
+  );
+}
+
+const TYPE_MAP: Record<Item["type"], string> = {
+  link: "lr-x",
+  file: "-r--",
+};
+
+type Item = {
+  name: string;
+  style: string;
+  size: number;
+  date: string;
+  count: number;
+  type: "link" | "file";
+  url: string;
+};
